938a11fb85c315d9b2a70adb2b14a052bfb0b469
kylejschulz <kylschulz@gmail.com>
2021-01-20 12:59:14 -0500

    finish up iteration 3

--- lib/market.rb
+++ lib/market.rb
@@ -1,9 +1,12 @@
+require 'date'
 class Market
   attr_reader :name,
-              :vendors
+              :vendors,
+              :date
   def initialize(name)
     @name = name
     @vendors = []
+    @date = (Time.now).strftime("%m/%d/%Y")
   end
 
   def add_vendor(vendor)
@@ -33,21 +36,46 @@ class Market
     hash = Hash.new
     @vendors.each do |vendor|
       vendor.inventory.each do |item, quantity|
-        # if hash[item][:quantity].nil?
-        #   hash[item][:quantity] = 0
-        # end
-        # if hash[item][:vendors].nil?
-        #   hash[item][:vendors] = []
-    hash[item] = {
-                  quantity: 0,
-                  vendors: []
-                  }
-                  hash[item] += quantity
-                  hash[item] << vendor
-
-
-
+        if hash[item].nil?
+          hash[item] = Hash.new
+        end
+        if hash[item][:quantity].nil?
+          hash[item][:quantity] = quantity
+        else
+          hash[item][:quantity] += quantity
+        end
+        if hash[item][:vendors].nil?
+          hash[item][:vendors] = [vendor]
+        else
+          hash[item][:vendors] << vendor
+        end
+      end
     end
+  hash
+  end
+
+  def sorted_list
+    items.map do |item|
+      item.name
+    end.sort
+  end
+
+  def overstocked_items
+    over_stocked = []
+    total_inventory.each do |item, qv|
+      over_stocked << item if qv[:quantity] > 50 && qv[:vendors].length > 1
+    end
+    over_stocked
+  end
+
+  def sell(item, quantity)
+    sale_item = total_inventory.select do |k, v|
+      k == item
+    end
+    if sale_item.value[:quantity] < quantity
+      false
+    else
+      
 
   end
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -11,10 +11,17 @@ class MarketTest < Minitest::Test
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
     @vendor1 = Vendor.new("Rocky Mountain Fresh")
     @market = Market.new("South Pearl Street Farmers Market")
+    @item5 = Item.new({name: 'Onion', price: '$0.25'})
+
   end
   def test_it_exists
     assert_instance_of Market, @market
   end
+
+  def test_it_has_date
+    assert_equal '01/20/2021', @market.date
+  end
+
   def test_it_has_attributes
     assert_equal "South Pearl Street Farmers Market", @market.name
     assert_equal [], @market.vendors
@@ -105,4 +112,57 @@ class MarketTest < Minitest::Test
     }
     assert_equal expected, @market.total_inventory
   end
+
+  def test_it_can_return_overstocked_items
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    #over 50 and sold by more than 2 vendors
+    assert_equal [@item1], @market.overstocked_items
+
+
+  end
+
+  def test_sorted_list
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    assert_equal ["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"], @market.sorted_list
+  end
+
+  def test_sell
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+    @vendor3.stock(@item1, 65)
+    @vendor3.stock(@item3, 10)
+
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    assert_equal true, @market.sell(@item1, 20)
+  end
+
 end
